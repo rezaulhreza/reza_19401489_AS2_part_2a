c12115b25f0a15b0001a040e3d33fcb3cc45abfb
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2020-06-16 12:56:02 +0100

    js update

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 const image = document.querySelector('.image');
+/* the first element in the document with the class ".image" is returned
+which will display the image
+*/
 const hover = document.querySelector('.hover');
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.close');
+/* the first element in the document with the class ".hover" is returned*/
 
+const modal = document.querySelector('.modal');
+/*the first element in the document with the class "modal" is returned
+Associated with the modal window, selecting the modal*/
+*/
+
+const close = document.querySelector('.close');
+/*the first element in the document with the class "close" is returned
+Associated with the close icon
+*/
 function show() {
   hover.classList.add('active');
   modal.classList.add('show');
 }
-
-function hide() {
-  hover.classList.remove('active');
-  modal.classList.remove('show');
-}
-
-image.addEventListener('click', show);
-close.addEventListener('click', hide);
