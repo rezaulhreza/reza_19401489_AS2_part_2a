6267ef89aa7ccb7ebf4e580d8a86e6356ba224e4
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2020-06-16 12:59:03 +0100

    show modal update

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ const hover = document.querySelector('.hover');
 const modal = document.querySelector('.modal');
 /*the first element in the document with the class "modal" is returned
 Associated with the modal window, selecting the modal*/
-*/
+
 
 const close = document.querySelector('.close');
 /*the first element in the document with the class "close" is returned
@@ -18,3 +18,9 @@ function show() {
   hover.classList.add('active');
   modal.classList.add('show');
 }
+/*after clicking on the icon the photo will show up as a modal window. adding the image into the modal*/
+
+
+
+image.addEventListener('click', show);
+close.addEventListener('click', hide);
